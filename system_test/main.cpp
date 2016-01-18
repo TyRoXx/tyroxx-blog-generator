8cc66da4859346ed9b49d257c89251dd5b4f9ad3
TyRoXx <tyroxxxx@gmail.com>
2016-01-18 20:48:18 +0100

    Windows cmd.exe is so bad that I have to script this trivial test in C++

---
+++ system_test/main.cpp
@@ -0,0 +1,29 @@
+#include <ventura/run_process.hpp>
+#include <ventura/file_operations.hpp>
+#include <silicium/sink/ostream_sink.hpp>
+#include <iostream>
+
+int main()
+{
+	try
+	{
+		ventura::absolute_path const here = ventura::get_current_working_directory(Si::throw_);
+		ventura::absolute_path const generator = here / ventura::relative_path("html_generator.exe");
+		std::vector<Si::noexcept_string> arguments;
+		arguments.emplace_back(to_utf8_string(here));
+		auto output = Si::Sink<char, Si::success>::erase(Si::ostream_ref_sink(std::cout));
+		int const rc =
+		    ventura::run_process(generator, arguments, here, output, {}, ventura::environment_inheritance::inherit)
+		        .get();
+		if (rc != 0)
+		{
+			std::cerr << generator << " " << arguments[0] << " failed with " << rc << '\n';
+			return 1;
+		}
+	}
+	catch (boost::system::system_error const &ex)
+	{
+		std::cerr << ex.what() << '\n';
+		return 1;
+	}
+}
