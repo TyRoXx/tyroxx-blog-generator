20464d90c6a7e5689e32facd9a10e4c124e686e2
TyRoXx <tyroxxxx@gmail.com>
2016-01-31 04:25:50 +0100

    import a code snippet from a file

--- html_generator/main.cpp
+++ html_generator/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <ventura/open.hpp>
 #include <ventura/file_operations.hpp>
+#include <ventura/read_file.hpp>
 #include <silicium/sink/file_sink.hpp>
 #include <silicium/sink/throwing_sink.hpp>
 #include <silicium/html/tree.hpp>
@@ -51,8 +52,8 @@ namespace
 		return p(tag("a", attribute("href", protocol + address_without_protocol), text(address_without_protocol)));
 	}
 
-	template <std::size_t N>
-	auto make_code_snippet(char const(&code)[N])
+	template <class StringLike>
+	auto make_code_snippet(StringLike const &code)
 	{
 		using namespace Si::html;
 		std::size_t const lines = std::count(std::begin(code), std::end(code), '\n') + 1;
@@ -62,13 +63,16 @@ namespace
 			line_numbers += boost::lexical_cast<std::string>(i);
 			line_numbers += '\n';
 		}
-		return tag("pre", attribute("style", "float:left"), text(std::move(line_numbers))) +
-		       tag("pre", attribute("style", "float:left;margin-left:30px"), text(code));
+		return tag("div", tag("pre", attribute("style", "float:left"), text(std::move(line_numbers))) +
+		                      tag("pre", attribute("style", "float:left;margin-left:20px"), text(code)) +
+		                      tag("div", attribute("style", "clear:both"),
+		                          /*TODO: encode the div correctly in HTML*/ text(" ")));
 	}
 
-	boost::system::error_code generate_all_html(ventura::absolute_path const &existing_root)
+	boost::system::error_code generate_all_html(ventura::absolute_path const &snippets_source_code,
+	                                            ventura::absolute_path const &existing_output_root)
 	{
-		ventura::absolute_path const index_path = existing_root / ventura::relative_path("index.html");
+		ventura::absolute_path const index_path = existing_output_root / ventura::relative_path("index.html");
 		Si::error_or<Si::file_handle> const index =
 		    ventura::overwrite_file(ventura::safe_c_str(to_os_string(index_path)));
 		if (index.is_error())
@@ -77,10 +81,64 @@ namespace
 			return index.error();
 		}
 
+		auto const snippet_from_file = [&snippets_source_code](char const *name)
+		{
+			ventura::absolute_path const full_name = snippets_source_code / ventura::relative_path(name);
+			Si::variant<std::vector<char>, boost::system::error_code, ventura::read_file_problem> read_result =
+			    ventura::read_file(ventura::safe_c_str(to_os_string(full_name)));
+			std::vector<char> content;
+			Si::visit<void>(
+			    read_result,
+			    [&content](std::vector<char> &read_content)
+			    {
+				    content = std::move(read_content);
+				},
+			    [&full_name](boost::system::error_code const error)
+			    {
+				    boost::throw_exception(std::runtime_error("Could not read file " + to_utf8_string(full_name) +
+				                                              ": " + boost::lexical_cast<std::string>(error)));
+				},
+			    [&full_name](ventura::read_file_problem const problem)
+			    {
+				    switch (problem)
+				    {
+				    case ventura::read_file_problem::file_too_large_for_memory:
+					    boost::throw_exception(
+					        std::runtime_error("File " + to_utf8_string(full_name) + " cannot be read into memory"));
+
+				    case ventura::read_file_problem::concurrent_write_detected:
+					    boost::throw_exception(
+					        std::runtime_error("File " + to_utf8_string(full_name) +
+					                           " cannot be read because it seems to be accessed concurrently"));
+				    }
+				});
+
+			std::string clean;
+			for (char c : content)
+			{
+				switch (c)
+				{
+				case '\t':
+					clean += "    ";
+					break;
+
+				case '\r':
+					break;
+
+				default:
+					clean += c;
+					break;
+				}
+			}
+			return make_code_snippet(clean);
+		};
+
 		Si::file_sink index_sink(index.get().handle);
 		using namespace Si::html;
 		char const title[] = "TyRoXx' blog";
+
 		auto articles = h2(text("Articles")) + text("Sorry, there are no finished articles yet.");
+
 		auto four_spaces =
 		    h3(tag("a", anchor_attributes("one-tab-is-four-spaces"), text("One tab is four spaces"))) +
 		    p(text("The following program demonstrates that one tab is in fact equivalent to four spaces:")) +
@@ -95,7 +153,19 @@ namespace
 		      text("Both ways achieve exactly the same result: Indenting by four characters. Only the characters "
 		           "used for indentation are different. The old argument has "
 		           "finally been settled."));
-		auto drafts = h2(text("Drafts")) + std::move(four_spaces);
+
+		auto throwing_constructor =
+		    h3(tag("a", anchor_attributes("throwing-constructor"), text("Throwing from a constructor"))) +
+		    snippet_from_file("throwing_constructor_0.cpp");
+		auto drafts = h2(text("Drafts")) + std::move(throwing_constructor) + std::move(four_spaces);
+
+		auto todo =
+		    h2(text("Technical to do list")) +
+		    tag("ul",
+		        tag("li", text("compile the code snippets")) + tag("li", text("color the code snippets")) +
+		            tag("li", text("clang-format the code snippets")) +
+		            tag("li", text("do the line numbers in a better way (use float correctly or use something else)")));
+
 		auto links = link("https://", "github.com/TyRoXx") + link("https://", "twitter.com/tyroxxxx") +
 		             link("mailto:", "tyroxxxx@gmail.com");
 		auto style = "body {\n"
@@ -108,7 +178,8 @@ namespace
 		             "}\n";
 		auto head = tag("head", tag("meta", attribute("charset", "utf-8"), empty) + tag("title", text(title)) +
 		                            tag("style", text(style)));
-		auto body = tag("body", h1(text(title)) + std::move(links) + std::move(articles) + std::move(drafts));
+		auto body =
+		    tag("body", h1(text(title)) + std::move(links) + std::move(articles) + std::move(drafts) + std::move(todo));
 		auto const document = raw("<!DOCTYPE html>") + tag("html", std::move(head) + std::move(body));
 		auto erased_sink = Si::Sink<char, Si::success>::erase(Si::make_throwing_sink(index_sink));
 		try
@@ -172,7 +243,10 @@ int main(int argc, char **argv)
 	}
 
 	{
-		boost::system::error_code const ec = generate_all_html(*output_root);
+		boost::system::error_code const ec =
+		    generate_all_html(*ventura::parent(*ventura::parent(*ventura::absolute_path::create(__FILE__))) /
+		                          ventura::relative_path("snippets"),
+		                      *output_root);
 		if (!!ec)
 		{
 			std::cerr << ec << '\n';
---
+++ snippets/throwing_constructor_0.cpp
@@ -0,0 +1,21 @@
+struct widget
+{
+	explicit widget(std::string const &important_file)
+	{
+		std::ifstream file(important_file, std::ios::binary);
+		important_data.insert(
+			important_data.begin(),
+		    std::istreambuf_iterator<char>(file),
+			std::istreambuf_iterator<char>());
+		if (!file)
+		{
+			throw std::runtime_error(
+				"Could not read file " +
+      			important_file);
+		}
+	}
+
+private:
+
+	std::vector<char> important_data;
+};
