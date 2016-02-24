a4f799555e8fa4adcf4edbd99a4061e301a76fd0
TyRoXx <tyroxxxx@gmail.com>
2016-02-24 21:44:57 +0100

    clang-format the snippets, too

--- html_generator/main.cpp
+++ html_generator/main.cpp
@@ -12,7 +12,8 @@ namespace
 	template <std::size_t N>
 	auto anchor_attributes(char const(&name)[N])
 	{
-		return Si::html::attribute("name", name) + Si::html::attribute("href", std::string("#") + name);
+		return Si::html::attribute("name", name) +
+		       Si::html::attribute("href", std::string("#") + name);
 	}
 
 	template <class Element>
@@ -24,7 +25,8 @@ namespace
 	template <class Element, class Attributes>
 	auto p(Attributes &&attributes, Element &&content)
 	{
-		return Si::html::tag("p", std::forward<Attributes>(attributes), std::forward<Element>(content));
+		return Si::html::tag("p", std::forward<Attributes>(attributes),
+		                     std::forward<Element>(content));
 	}
 
 	template <class Element>
@@ -46,34 +48,44 @@ namespace
 	}
 
 	template <std::size_t N>
-	auto link(std::string const &protocol, char const(&address_without_protocol)[N])
+	auto link(std::string const &protocol,
+	          char const(&address_without_protocol)[N])
 	{
 		using namespace Si::html;
-		return p(tag("a", attribute("href", protocol + address_without_protocol), text(address_without_protocol)));
+		return p(tag("a",
+		             attribute("href", protocol + address_without_protocol),
+		             text(address_without_protocol)));
 	}
 
 	template <class StringLike>
 	auto make_code_snippet(StringLike const &code)
 	{
 		using namespace Si::html;
-		std::size_t const lines = std::count(std::begin(code), std::end(code), '\n') + 1;
+		std::size_t const lines =
+		    std::count(std::begin(code), std::end(code), '\n') + 1;
 		std::string line_numbers;
 		for (std::size_t i = 1; i <= lines; ++i)
 		{
 			line_numbers += boost::lexical_cast<std::string>(i);
 			line_numbers += '\n';
 		}
-		return tag("div", attribute("style", "white-space:nowrap"),
-		           tag("pre", attribute("style", "display:inline-block"), text(std::move(line_numbers))) +
-		               tag("pre", attribute("style", "display:inline-block;margin-left:20px"), text(code)));
+		return tag(
+		    "div", attribute("style", "white-space:nowrap"),
+		    tag("pre", attribute("style", "display:inline-block"),
+		        text(std::move(line_numbers))) +
+		        tag("pre",
+		            attribute("style", "display:inline-block;margin-left:20px"),
+		            text(code)));
 	}
 
-	boost::system::error_code generate_all_html(ventura::absolute_path const &snippets_source_code,
-	                                            ventura::absolute_path const &existing_output_root)
+	boost::system::error_code
+	generate_all_html(ventura::absolute_path const &snippets_source_code,
+	                  ventura::absolute_path const &existing_output_root)
 	{
-		ventura::absolute_path const index_path = existing_output_root / ventura::relative_path("index.html");
-		Si::error_or<Si::file_handle> const index =
-		    ventura::overwrite_file(ventura::safe_c_str(to_os_string(index_path)));
+		ventura::absolute_path const index_path =
+		    existing_output_root / ventura::relative_path("index.html");
+		Si::error_or<Si::file_handle> const index = ventura::overwrite_file(
+		    ventura::safe_c_str(to_os_string(index_path)));
 		if (index.is_error())
 		{
 			std::cerr << "Could not overwrite file " << index_path << '\n';
@@ -82,9 +94,12 @@ namespace
 
 		auto const snippet_from_file = [&snippets_source_code](char const *name)
 		{
-			ventura::absolute_path const full_name = snippets_source_code / ventura::relative_path(name);
-			Si::variant<std::vector<char>, boost::system::error_code, ventura::read_file_problem> read_result =
-			    ventura::read_file(ventura::safe_c_str(to_os_string(full_name)));
+			ventura::absolute_path const full_name =
+			    snippets_source_code / ventura::relative_path(name);
+			Si::variant<std::vector<char>, boost::system::error_code,
+			            ventura::read_file_problem> read_result =
+			    ventura::read_file(
+			        ventura::safe_c_str(to_os_string(full_name)));
 			std::vector<char> content;
 			Si::visit<void>(
 			    read_result,
@@ -94,21 +109,24 @@ namespace
 				},
 			    [&full_name](boost::system::error_code const error)
 			    {
-				    boost::throw_exception(std::runtime_error("Could not read file " + to_utf8_string(full_name) +
-				                                              ": " + boost::lexical_cast<std::string>(error)));
+				    boost::throw_exception(std::runtime_error(
+				        "Could not read file " + to_utf8_string(full_name) +
+				        ": " + boost::lexical_cast<std::string>(error)));
 				},
 			    [&full_name](ventura::read_file_problem const problem)
 			    {
 				    switch (problem)
 				    {
 				    case ventura::read_file_problem::file_too_large_for_memory:
-					    boost::throw_exception(
-					        std::runtime_error("File " + to_utf8_string(full_name) + " cannot be read into memory"));
+					    boost::throw_exception(std::runtime_error(
+					        "File " + to_utf8_string(full_name) +
+					        " cannot be read into memory"));
 
 				    case ventura::read_file_problem::concurrent_write_detected:
-					    boost::throw_exception(
-					        std::runtime_error("File " + to_utf8_string(full_name) +
-					                           " cannot be read because it seems to be accessed concurrently"));
+					    boost::throw_exception(std::runtime_error(
+					        "File " + to_utf8_string(full_name) +
+					        " cannot be read because it seems to be accessed "
+					        "concurrently"));
 				    }
 				});
 
@@ -136,18 +154,23 @@ namespace
 		using namespace Si::html;
 		char const title[] = "TyRoXx' blog";
 
-		auto articles = h2(text("Articles")) + text("Sorry, there are no finished articles yet.");
+		auto articles = h2(text("Articles")) +
+		                text("Sorry, there are no finished articles yet.");
 
 		auto throwing_constructor =
-		    h3(tag("a", anchor_attributes("throwing-constructor"), text("Throwing from a constructor"))) +
+		    h3(tag("a", anchor_attributes("throwing-constructor"),
+		           text("Throwing from a constructor"))) +
 		    snippet_from_file("throwing_constructor_0.cpp");
 		auto drafts = h2(text("Drafts")) + std::move(throwing_constructor);
 
-		auto todo = h2(text("Technical to do list")) + tag("ul", tag("li", text("compile the code snippets")) +
-		                                                             tag("li", text("color the code snippets")) +
-		                                                             tag("li", text("clang-format the code snippets")));
+		auto todo =
+		    h2(text("Technical to do list")) +
+		    tag("ul", tag("li", text("compile the code snippets")) +
+		                  tag("li", text("color the code snippets")) +
+		                  tag("li", text("clang-format the code snippets")));
 
-		auto links = link("https://", "github.com/TyRoXx") + link("https://", "twitter.com/tyroxxxx") +
+		auto links = link("https://", "github.com/TyRoXx") +
+		             link("https://", "twitter.com/tyroxxxx") +
 		             link("mailto:", "tyroxxxx@gmail.com");
 		auto style = "body {\n"
 		             "	font-size: 16px;\n"
@@ -157,12 +180,16 @@ namespace
 		             "p {\n"
 		             "	line-height: 1.6;\n"
 		             "}\n";
-		auto head = tag("head", tag("meta", attribute("charset", "utf-8"), empty) + tag("title", text(title)) +
-		                            tag("style", text(style)));
-		auto body =
-		    tag("body", h1(text(title)) + std::move(links) + std::move(articles) + std::move(drafts) + std::move(todo));
-		auto const document = raw("<!DOCTYPE html>") + tag("html", std::move(head) + std::move(body));
-		auto erased_sink = Si::Sink<char, Si::success>::erase(Si::make_throwing_sink(index_sink));
+		auto head = tag(
+		    "head", tag("meta", attribute("charset", "utf-8"), empty) +
+		                tag("title", text(title)) + tag("style", text(style)));
+		auto body = tag("body", h1(text(title)) + std::move(links) +
+		                            std::move(articles) + std::move(drafts) +
+		                            std::move(todo));
+		auto const document = raw("<!DOCTYPE html>") +
+		                      tag("html", std::move(head) + std::move(body));
+		auto erased_sink = Si::Sink<char, Si::success>::erase(
+		    Si::make_throwing_sink(index_sink));
 		try
 		{
 			document.generate(erased_sink);
@@ -181,8 +208,9 @@ int main(int argc, char **argv)
 	std::string output_option;
 
 	boost::program_options::options_description desc("Allowed options");
-	desc.add_options()("help", "produce help message")("output", boost::program_options::value(&output_option),
-	                                                   "a directory to put the HTML files into");
+	desc.add_options()("help", "produce help message")(
+	    "output", boost::program_options::value(&output_option),
+	    "a directory to put the HTML files into");
 
 	boost::program_options::positional_options_description positional;
 	positional.add("output", 1);
@@ -190,7 +218,11 @@ int main(int argc, char **argv)
 	try
 	{
 		boost::program_options::store(
-		    boost::program_options::command_line_parser(argc, argv).options(desc).positional(positional).run(), vm);
+		    boost::program_options::command_line_parser(argc, argv)
+		        .options(desc)
+		        .positional(positional)
+		        .run(),
+		    vm);
 	}
 	catch (boost::program_options::error const &ex)
 	{
@@ -206,16 +238,19 @@ int main(int argc, char **argv)
 		return 1;
 	}
 
-	Si::optional<ventura::absolute_path> const output_root = ventura::absolute_path::create(output_option);
+	Si::optional<ventura::absolute_path> const output_root =
+	    ventura::absolute_path::create(output_option);
 	if (!output_root)
 	{
-		std::cerr << "The output directory must be given as an absolute path.\n";
+		std::cerr
+		    << "The output directory must be given as an absolute path.\n";
 		std::cerr << desc << "\n";
 		return 1;
 	}
 
 	{
-		boost::system::error_code const ec = ventura::create_directories(*output_root, Si::return_);
+		boost::system::error_code const ec =
+		    ventura::create_directories(*output_root, Si::return_);
 		if (!!ec)
 		{
 			std::cerr << ec << '\n';
@@ -225,7 +260,8 @@ int main(int argc, char **argv)
 
 	{
 		boost::system::error_code const ec =
-		    generate_all_html(*ventura::parent(*ventura::parent(*ventura::absolute_path::create(__FILE__))) /
+		    generate_all_html(*ventura::parent(*ventura::parent(
+		                          *ventura::absolute_path::create(__FILE__))) /
 		                          ventura::relative_path("snippets"),
 		                      *output_root);
 		if (!!ec)
--- system_test/main.cpp
+++ system_test/main.cpp
@@ -7,17 +7,22 @@ int main()
 {
 	try
 	{
-		ventura::absolute_path const here = ventura::get_current_working_directory(Si::throw_);
-		ventura::absolute_path const generator = here / ventura::relative_path("html_generator.exe");
+		ventura::absolute_path const here =
+		    ventura::get_current_working_directory(Si::throw_);
+		ventura::absolute_path const generator =
+		    here / ventura::relative_path("html_generator.exe");
 		std::vector<Si::noexcept_string> arguments;
 		arguments.emplace_back(to_utf8_string(here));
-		auto output = Si::Sink<char, Si::success>::erase(Si::ostream_ref_sink(std::cout));
+		auto output =
+		    Si::Sink<char, Si::success>::erase(Si::ostream_ref_sink(std::cout));
 		int const rc =
-		    ventura::run_process(generator, arguments, here, output, {}, ventura::environment_inheritance::inherit)
+		    ventura::run_process(generator, arguments, here, output, {},
+		                         ventura::environment_inheritance::inherit)
 		        .get();
 		if (rc != 0)
 		{
-			std::cerr << generator << " " << arguments[0] << " failed with " << rc << '\n';
+			std::cerr << generator << " " << arguments[0] << " failed with "
+			          << rc << '\n';
 			return 1;
 		}
 	}
