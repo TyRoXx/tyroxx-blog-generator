eeffc59d7e903554e7bb842cd5881fe18fb092d5
mamazu <maxo.pech@gmail.com>
2017-03-30 13:11:48 +0200

    Added more test cased to check br and paragraph tags

--- tests/tags.cpp
+++ tests/tags.cpp
@@ -9,6 +9,26 @@ BOOST_AUTO_TEST_CASE(tags_link)
 	auto tree = tags::link("https://", "google.com");
 	tree.generate(erased_html_sink);
 	BOOST_CHECK_EQUAL(
-	    R"(<a href="https://google.com" target="_blank">google.com</a>)",
+	    "<a href=\"https://google.com\" target=\"_blank\">google.com</a>",
 	    html_generated);
+}
+
+BOOST_AUTO_TEST_CASE(paragraph)
+{
+	std::string html_generated;
+	auto erased_html_sink = Si::Sink<char, Si::success>::erase(
+	    Si::make_container_sink(html_generated));
+	auto tree = tags::p("Testing p tag");
+	tree.generate(erased_html_sink);
+	BOOST_CHECK_EQUAL("<p>Testing p tag</p>", html_generated);
+}
+
+BOOST_AUTO_TEST_CASE(break_row)
+{
+	std::string html_generated;
+	auto erased_html_sink = Si::Sink<char, Si::success>::erase(
+	    Si::make_container_sink(html_generated));
+	auto tree = tags::br(tags::cl("clear"));
+	tree.generate(erased_html_sink);
+	BOOST_CHECK_EQUAL("<br class=\"clear\"/>", html_generated);
 }
