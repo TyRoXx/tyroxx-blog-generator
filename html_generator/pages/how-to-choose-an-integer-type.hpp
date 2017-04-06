8465818462d400b624b3237322176d886aa1a6e2
mamazu <maxo.pech@gmail.com>
2017-04-06 23:02:17 +0200

    Put the free text in a paragraph

--- html_generator/pages/how-to-choose-an-integer-type.hpp
+++ html_generator/pages/how-to-choose-an-integer-type.hpp
@@ -1,7 +1,8 @@
 tags::h3(tags::link("#", "how-to-choose-an-integer-type",
                     "How to choose an integer type")) +
     tags::p("(created 2017-04-05, updated 2017-04-05)") +
-    text("Prefer portable types like ") + inline_code("std::uint32_t") +
+
+    Si::html::tag("p", text("Prefer portable types like ") + inline_code("std::uint32_t") +
     text(" over the types without an explicit range. Use ") +
     inline_code("int") + text(", ") + inline_code("long") + text(", ") +
     inline_code("long long") +
@@ -10,29 +11,30 @@ tags::h3(tags::link("#", "how-to-choose-an-integer-type",
     inline_code("int") +
     text(" etc are not the same size and range on every platform. This makes "
          "them hard to use even for C++ experts. They are neither necessary "
-         "nor sufficient for most practical situations.") +
+         "nor sufficient for most practical situations.")) +
+
     tags::p(
         "In most cases you already know the expected range of values. So why "
         "not use the right integer type for this instead instead of "
         "cargo-culting it?") +
-    text("If you know that you are dealing with 64-bit file sizes, document "
+    Si::html::tag("p", text("If you know that you are dealing with 64-bit file sizes, document "
          "this assumption in an unambiguous way. There is no reason for "
          "using ") +
     inline_code("int") + text(" or ") + inline_code("long long") +
     text(" in this case. Using the wrong type leads to unportable code and "
-         "misunderstandings.") +
+         "misunderstandings.")) +
     snippet_from_file(
         snippets_source_code,
         ventura::relative_path("how-to-choose-an-integer-type-0.cpp")) +
 
-    text("One of the few valid use cases for the built-in types is overloading "
+    tags::p("One of the few valid use cases for the built-in types is overloading "
          "a function for all integer types that exist:") +
     snippet_from_file(
         snippets_source_code,
         ventura::relative_path("how-to-choose-an-integer-type-1.cpp")) +
-    text("This kind of code should be fairly uncommon though.") + tags::p("") +
+    tags::p("This kind of code should be fairly uncommon though.") +
 
-    text("The other major kind of integer types are the pointery ones: ") +
+    Si::html::tag("p", text("The other major kind of integer types are the pointery ones: ") +
     inline_code("std::size_t") + text(", ") + inline_code("std::ptrdiff_t") +
     text(" and ") + inline_code("std::uintptr_t") +
     text(". They are used when dealing with memory on the current machine. ") +
@@ -51,17 +53,21 @@ tags::h3(tags::link("#", "how-to-choose-an-integer-type",
          "make use of.") +
     text(
         " This is a very small function how it would be written by someone who "
-        "does not waste a single thought on choosing the right type.") +
+        "does not waste a single thought on choosing the right type.")) +
+
     snippet_from_file(
         snippets_source_code,
         ventura::relative_path("how-to-choose-an-integer-type-2.cpp")) +
-    text("These are my thoughts when I read this function. I tried to mark the "
+
+    tags::p("These are my thoughts when I read this function. I tried to mark the "
          "WTF moments so that you can calculate the WTFs per line metric as a "
          "take-home excercise if you like.") +
+
     snippet_from_file(
         snippets_source_code,
         ventura::relative_path("how-to-choose-an-integer-type-3.cpp")) +
-    text("I only have to change one little thing to make all these WTFs go "
+
+    tags::p("I only have to change one little thing to make all these WTFs go "
          "away:") +
     snippet_from_file(
         snippets_source_code,
