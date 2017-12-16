2d56dbf5020c5b3dcacdb21783f2f929aaa543a7
TyRoXx <tyroxxxx@gmail.com>
2017-12-16 18:52:54 +0100

    correct Twitter user name

--- html_generator/pages/footer.hpp
+++ html_generator/pages/footer.hpp
@@ -2,6 +2,6 @@ tags::footer(
     tags::ul(tags::li(tags::a(tags::href("https://github.com/TyRoXx"),
                               tags::p("GitHub Link"))) +
              tags::li(tags::a(tags::href("https://twitter.com/tyroxxxx"),
-                              tags::p("Twitter @tyroxxx"))) +
+                              tags::p("Twitter @tyroxxxx"))) +
              tags::li(tags::a(tags::href("mailto:tyroxxxx@gmail.com"),
                               tags::p("tyroxxxx@gmail.com")))))
